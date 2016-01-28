bc2255d3cf37898cc85eb7deab698fdce8322044
sndpkiran <sndpkiran@gmail.com>
2016-01-29 01:17:06 +0530

    Improved routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-
-    resources :comments
-  resources :posts do 
-  end
   root "posts#index"
+  
+  resources :posts
+  
+  resources :comments
+  
   devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
